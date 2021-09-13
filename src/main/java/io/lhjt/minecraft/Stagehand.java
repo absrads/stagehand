cdfb68150ab680fcb0cf9c9c3f9e7a11ca34a83a
Jared L <48422312+lhjt@users.noreply.github.com>
2021-09-13 15:54:23 +1000

    chore(release): start dev iteration `0.6.1-alpha`

--- src/main/java/io/lhjt/minecraft/Stagehand.java
+++ src/main/java/io/lhjt/minecraft/Stagehand.java
@@ -16,7 +16,7 @@ import io.lhjt.minecraft.commands.CommandManager;
 import io.lhjt.minecraft.modules.RandomSpawn;
 import io.lhjt.minecraft.utils.EventLoader;
 
-@Plugin(name = "stagehand", version = "0.6.0")
+@Plugin(name = "stagehand", version = "0.6.1-alpha")
 @ApiVersion(value = Target.v1_17)
 @Author(value = "lhjt")
 @Description(value = "A management plugin to set the scene of the server.")
